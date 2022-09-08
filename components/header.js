8c4f2366c1490cd87a9123f6cdd303755b0c8db9
Raphael Motta <rapha-sm2015@hotmail.com>
2022-09-08 16:42:12 -0300

    Initial commit

---
+++ components/header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <div className = {`h-48 bg-cyan-500`}>
+                Teste
+            </div>
+        </header>
+    )
+}

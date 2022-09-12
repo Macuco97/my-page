16e29938d1a1e4c2d1240ba68c16953fc0c38ba6
unknown <rapha-sm2015@hotmail.com>
2022-09-12 07:41:01 -0300

    Set Height for Content

--- components/contain.js
+++ components/contain.js
@@ -1,5 +1,5 @@
 export default function Contain({activeImage, setActiveImage}) {
-const container = `flex flex-box justify-center justify-items-center mx-8 bg-slate-200 shadow-slate-500/50 shadow-2xl p-8 mt-12 w-80`
+const container = `flex flex-box justify-center justify-items-center mx-8 bg-slate-200 shadow-slate-500/50 shadow-2xl p-8 mt-12 w-80 h-64`
 const manutencaoContents = `overflow-hidden ${activeImage === "manutencao" ? `transition opacity-100 duration-500` : `transition opacity-0 max-h-0 max-w-0`}`
 const siteContents = `overflow-hidden ${activeImage === 'site' ? `transition opacity-100 duration-500` : `transition opacity-0 max-h-0 max-w-0`}`
 const discContents = `overflow-hidden ${activeImage === 'disc' ? `transition opacity-100 duration-500` : `transition opacity-0 max-h-0 max-w-0`}`

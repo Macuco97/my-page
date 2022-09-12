69d48691d79fe9f7c688fe2b0082009852333666
unknown <rapha-sm2015@hotmail.com>
2022-09-12 04:29:11 -0300

    Add subtitle

--- components/header.js
+++ components/header.js
@@ -1,10 +1,14 @@
-const header = "h-14 w-screen font-serif text-3xl p-2 text-center mt-12"
+const headerTitle = "h-14 w-screen font-serif text-3xl p-2 text-center mt-12"
+const headerSubTitle = "w-screen font-serif text-1xl p-2 text-center underline underline-offset-8"
 
 export default function Header() {
     return (
         <header>
-            <div className = {header}>
+            <div className = {headerTitle}>
                InfoTech Soluções
+            </div>
+            <div className = {headerSubTitle}>
+                ESCOLHA ABAIXO A SOLUÇÃO DESEJADA
             </div>
         </header>
     )

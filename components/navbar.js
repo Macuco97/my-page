6c23950aa55fa1aca0a790ba2c59388e89a01b50
unknown <rapha-sm2015@hotmail.com>
2022-09-12 06:16:55 -0300

    Icon shadow color apply

--- components/navbar.js
+++ components/navbar.js
@@ -3,9 +3,9 @@ import { useState } from 'react'
 
 export default function Navbar({activeImage, setActiveImage}) {
     const navbar = `mt-8 flex flex-row w-screen justify-center self-end`
-    const imageManutencao =  activeImage === 'manutencao' ? ' duration-500 translate-y-6 shadow-2xl shadow-green-500/50 border border-5 rounded-md border-solid' : 'shadow-2xl duration-500 translate-y-0 opacity-50 border border-5 rounded-md border-solid'
-    const imageSite = activeImage === 'site' ? 'duration-500 translate-y-6 shadow-2xl shadow-green-500/50 border border-5 rounded-md border-solid' : 'shadow-2xl duration-500 translate-y-0 opacity-50 border border-5 rounded-md border-solid'
-    const imageDisc = activeImage === 'disc' ? 'duration-500 translate-y-6 shadow-2xl shadow-green-500/50 border border-5 rounded-md border-solid' : 'shadow-2xl opacity-50 duration-500 translate-y-0 border border-5 rounded-md border-solid'
+    const imageManutencao =  activeImage === 'manutencao' ? ' duration-500 translate-y-6 shadow-2xl shadow-green-500/50 rounded-md' : 'shadow-2xl duration-500 translate-y-0 opacity-50 rounded-md'
+    const imageSite = activeImage === 'site' ? 'duration-500 translate-y-6 shadow-2xl shadow-green-500/50 rounded-md' : 'shadow-2xl duration-500 translate-y-0 opacity-50 rounded-md'
+    const imageDisc = activeImage === 'disc' ? 'duration-500 translate-y-6 shadow-2xl shadow-green-500/50 rounded-md' : 'shadow-2xl opacity-50 duration-500 translate-y-0 rounded-md'
     const hr = "border-5 border-solid"
     
 

80c52576d665ccef354da25d4e9fcf0427635d5a
unknown <rapha-sm2015@hotmail.com>
2022-09-13 06:05:23 -0300

    Change whatsapp icon position 2

--- components/footer.js
+++ components/footer.js
@@ -8,7 +8,7 @@ const footer = "flex flex-row justify-center"
     return ( 
     <footer className = {footer}>
         <Link href = "http://wa.me/5521999611299">
-            <span className = "ml-48 mt-19 animate-bounce">
+            <span className = "ml-72 mt-12 animate-bounce">
                 <Image className = {``} src = "/whatsapp-logo.png" width = "50" height = "50"/>
             </span>
         </Link>

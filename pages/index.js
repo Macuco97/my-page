7b1db544d9d811f56785731b6a6e21043552bf1f
unknown <rapha-sm2015@hotmail.com>
2022-09-15 13:16:49 -0300

    Change page padding for margin in each element

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ import Footer from '../components/footer'
 import Link from 'next/link'
 export default function Home() {
     return (
-        <div className = {`p-8 bg-slate-100 min-h-screen w-screen m-0`}>
+        <div className = {`bg-slate-100 h-screen w-screen`}>
             <div className = {`font-semibold font-sans flex flex-col`}>    
-                <div className = {`text-4xl`}>Olá !</div>
-                <div className = {`text-2xl`}>Seja bem-vindo</div>
-                <div className = {`text-xl`}>Aqui é o local onde você encontrará soluções em TI para alavancar seus negócios</div>
-                <div className = {`underline underline-offset-8 mt-10`}>Veja mais sobre</div>
+                <div className = {`text-4xl ml-8 mt-8`}>Olá !</div>
+                <div className = {`text-2xl ml-8`}>Seja bem-vindo</div>
+                <div className = {`text-xl ml-8`}>Aqui é o local onde você encontrará soluções em TI para alavancar seus negócios</div>
+                <div className = {`underline underline-offset-8 mt-10 ml-8`}>Veja mais sobre</div>
                 <div className = {`mt-4`}>
                     <Button href = "/manutencao">manutenção de computadores</Button>
                     <Button href = "/webdev">desenvolvimento de sites</Button>

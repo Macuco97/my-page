458242254991be625bd38de27403bec85b92ce01
unknown <rapha-sm2015@hotmail.com>
2022-09-22 10:59:03 -0300

    Fixing margin in webdev page

--- pages/webdev.js
+++ pages/webdev.js
@@ -3,17 +3,17 @@ import Footer from "../components/footer.js"
 
 export default function Webdev() {
     return ( 
-        <div className = {`container bg-slate-100 p-12 min-h-screen min-w-screen font-semibold font-serif`}>
-            <div className = {`text-3xl`}>
+        <div className = {`container bg-slate-100 min-h-screen min-w-screen font-semibold font-serif`}>
+            <div className = {`text-3xl pt-4 pl-4`}>
                 Sobre
             </div>
-            <div className = {`font-medium`}>
+            <div className = {`font-medium pl-4`}>
                 Criação de Sites com objetivo de criar uma marca digital do seu negócio e ser localizado no Google. O site é construído visando rankear as melhores posições de pesquisa do Google.
             </div>
-            <div className = {`text-xl underline underline-offset-8 uppercase mt-8`}> 
+            <div className = {`pl-4 text-xl underline underline-offset-8 uppercase mt-8`}> 
                 Veja o portfólio abaixo
             </div>
-            <div className = {`flex flex-col`}>
+            <div className = {`pl-4 flex flex-col`}>
                 <Card
                 href = "https://queue-manager-rho.vercel.app/">
                     <div>Queue Manager</div>

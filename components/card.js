3c1786fb48dcecfd66688372ccf04b415eb13e3e
COM Ingest <comingest@gsatht.com>
2022-09-30 21:35:36 -0300

    Change card bg color

--- components/card.js
+++ components/card.js
@@ -5,10 +5,10 @@ import Link from 'next/link'
 export default function Card({children, href, stacks}) { 
     const style = {
         stacks: "flex flex-row px-2 gap-2 flex-wrap justify-center my-2",
-        stack: "uppercase bg-black text-white p-1 rounded shadow-md shadow-black"
+        stack: "uppercase bg-black text-white p-1 rounded shadow shadow-black"
     }
     return (    
-            <div className = {`mt-4 w-64 shadow-xl bg-transparent flex flex-col`}>
+            <div className = {`mt-4 w-64 shadow-xl shadow-slate-400 bg-transparent flex flex-col py-4 bg-slate-300`}>
                 <div className = {`px-4`}>
                     <div className = {`font-bold underline underline-offset-2`}>{children[0]}</div>
                     <div>{children[1]}</div>
